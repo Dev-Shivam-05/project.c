896d999952e4af0df879db384322743616c4b1dd
kundan-14112 <panditkundanm@gmail.com>
2024-10-11 16:07:08 +0530

    first commit

---
+++ multi.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main() {
+	int a, square;
+	a=99;
+	printf("- - - - - - - - - - - - - - - -\n\n");
+	square=a * a;
+	printf("   The Square of a = %d\n\n",square);
+	printf("- - - - - - - - - - - - - - - -\n");
+}
