97ba9236cbd0e49c910769b7357d9beda1aa0fa1
Shivam <shivambhadoriya1605@gmail.com>
2025-05-20 21:47:29 +0530

    Update area.c

--- area.c
+++ area.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-	int main(){
-	int b;
-	int h;
+int main(){
+	int b,h;
 	int result;
+
 	printf("Enter Breadth: ");
 	scanf("%d",&b);
+	
 	printf("Enter Height: ");
 	scanf("%d",&h);
+	
 	result = 0.5*b*h;
+	
 	printf("Area of Traiangle is:  %d",result);
+	
 	return 0;
 }    
